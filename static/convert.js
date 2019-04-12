7e0e3c9dba17a704c7c3b58e4dac35442f12892b
RobinStut <robin.stut@hva.nl>
2019-04-12 11:15:21 +0200

    iteration 2

---
+++ static/convert.js
@@ -0,0 +1,49 @@
+
+
+(function ($) {
+  const up = [192, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 189, 187, 8, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219, 221, 38];
+  const down = [20, 65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 220, 16, 192, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191, 16, 17, 18, 91, 93, 18, 40];
+
+  let spaceCount = 0;
+
+
+  const grabHandle = $("a");
+  const rows = $("li[aria-grabbed]");
+
+  return grabHandle.on("keyup", function (event) {
+    const $this = $(this);
+    const $row = $this.parent();
+
+    if (event.which === 32 && spaceCount === 0) {
+      spaceCount = 1;
+      console.log(spaceCount);
+      rows.attr("aria-grabbed", false);
+      return $row.attr('aria-grabbed', true);
+    }
+
+    if (up.includes(event.which)) {
+      if ($row.attr('aria-grabbed') === "true") {
+        const prev = $row.prev();
+        if (prev) {
+          $row.insertBefore(prev);
+        }
+        return $this.focus();
+      }
+    }
+
+    if (down.includes(event.which)) {
+      if ($row.attr('aria-grabbed') === "true") {
+        const next = $row.next();
+        if (next) {
+          $row.insertAfter(next);
+        }
+        return $this.focus();
+      }
+    }
+    if (event.which === 13 || event.which === 27 || (event.which === 32 && spaceCount === 1)) {
+      spaceCount = 0;
+      return rows.attr("aria-grabbed", false);
+    }
+  });
+
+})(jQuery);
--- static/index.js
+++ static/index.js
@@ -1,73 +1,215 @@
 console.log("working on it");
 
-var dragged;
+// var dragged;
+//
+// /* events fired on the draggable target */
+// document.addEventListener("drag", function(event) {}, false);
+//
+// document.addEventListener(
+//   "dragstart",
+//   function(event) {
+//     // store a ref. on the dragged elem
+//     dragged = event.target;
+//     // make it half transparent
+//     event.target.style.opacity = 0.5;
+//   },
+//   false
+// );
+//
+// document.addEventListener(
+//   "dragend",
+//   function(event) {
+//     // reset the transparency
+//     event.target.style.opacity = "";
+//   },
+//   false
+// );
+//
+// /* events fired on the drop targets */
+// document.addEventListener(
+//   "dragover",
+//   function(event) {
+//     // prevent default to allow drop
+//     event.preventDefault();
+//   },
+//   false
+// );
+//
+// document.addEventListener(
+//   "dragenter",
+//   function(event) {
+//     // highlight potential drop target when the draggable element enters it
+//     if (event.target.className == "dropzone") {
+//       event.target.style.background = "purple";
+//     }
+//   },
+//   false
+// );
+//
+// document.addEventListener(
+//   "dragleave",
+//   function(event) {
+//     // reset background of potential drop target when the draggable element leaves it
+//     if (event.target.className == "dropzone") {
+//       event.target.style.background = "";
+//     }
+//   },
+//   false
+// );
+//
+// document.addEventListener(
+//   "drop",
+//   function(event) {
+//     // prevent default action (open as link for some elements)
+//     event.preventDefault();
+//     // move dragged elem to the selected drop target
+//     if (event.target.className == "dropzone") {
+//       event.target.style.background = "";
+//       dragged.parentNode.removeChild(dragged);
+//       event.target.appendChild(dragged);
+//     }
+//   },
+//   false
+// );
 
-/* events fired on the draggable target */
-document.addEventListener("drag", function(event) {}, false);
-
-document.addEventListener(
-  "dragstart",
+window.addEventListener(
+  "keydown",
   function(event) {
-    // store a ref. on the dragged elem
-    dragged = event.target;
-    // make it half transparent
-    event.target.style.opacity = 0.5;
-  },
-  false
-);
+    const tr = [
+      "Digit7",
+      "KeyY",
+      "KeyU",
+      "KeyI",
+      "KeyO",
+      "KeyP",
+      "BracketLeft",
+      "BracketRight",
+      "Enter",
+      "Backspace",
+      "Equal",
+      "Minus",
+      "Digit0",
+      "Digit9",
+      "Digit8"
+    ];
+    const dr = [
+      "KeyH",
+      "KeyJ",
+      "KeyK",
+      "KeyL",
+      "Semicolon",
+      "Quote",
+      "Backslash",
+      "KeyN",
+      "KeyM",
+      "Comma",
+      "Period",
+      "Slash",
+      "ShiftRight",
+      "MetaRight",
+      "AltRight",
+      "ArrowLeft",
+      "ArrowUp",
+      "ArrowDown",
+      "ArrowRight"
+    ];
+    const tl = [
+      "Digit6",
+      "Digit5",
+      "Digit4",
+      "Digit3",
+      "Digit2",
+      "Digit1",
+      "Backquote",
+      "Tab",
+      "KeyQ",
+      "KeyW",
+      "KeyE",
+      "KeyR",
+      "KeyT",
+      "Escape"
+    ];
+    const dl = [
+      "CapsLock",
+      "KeyA",
+      "KeyS",
+      "KeyD",
+      "KeyF",
+      "KeyG",
+      "KeyV",
+      "KeyC",
+      "KeyX",
+      "KeyZ",
+      "IntlBackslash",
+      "ShiftLeft",
+      "ControlLeft",
+      "AltLeft",
+      "MetaLeft"
+    ];
 
-document.addEventListener(
-  "dragend",
-  function(event) {
-    // reset the transparency
-    event.target.style.opacity = "";
-  },
-  false
-);
+    let status;
 
-/* events fired on the drop targets */
-document.addEventListener(
-  "dragover",
-  function(event) {
-    // prevent default to allow drop
-    event.preventDefault();
-  },
-  false
-);
-
-document.addEventListener(
-  "dragenter",
-  function(event) {
-    // highlight potential drop target when the draggable element enters it
-    if (event.target.className == "dropzone") {
-      event.target.style.background = "purple";
+    let str =
+      "KeyboardEvent: key='" + event.key + "' | code='" + event.code + "'";
+    // console.log(str);
+    if (event.code === "Space") {
+      console.log("space");
+    }
+    if (tr.includes(event.code)) {
+      console.log("TopRight " + event.code);
+    }
+    if (dr.includes(event.code)) {
+      console.log("DownRight " + event.code);
+    }
+    if (tl.includes(event.code)) {
+      console.log("TopLeft " + event.code);
+    }
+    if (dl.includes(event.code)) {
+      console.log("DownLeft " + event.code);
     }
   },
-  false
+  true
 );
 
-document.addEventListener(
-  "dragleave",
-  function(event) {
-    // reset background of potential drop target when the draggable element leaves it
-    if (event.target.className == "dropzone") {
-      event.target.style.background = "";
-    }
-  },
-  false
-);
+//
+// (($) ->
+//
+//     SPACE = 32
+//     UP = 38
+//     DOWN = 40
+//     ENTER = 13
+//     ESC = 27
+//
+//     grabHandle = $ "a"
+//     rows = $ "li[aria-grabbed]"
+//
+//     grabHandle.on "keyup", (event) ->
+//         $this = $ this
+//         $row = $this.parent()
+//         switch event.which
+//             when SPACE
+//                 # console.log('space', 'grabbed', $row.attr "aria-grabbed");
+//                 rows.attr("aria-grabbed", false)
+//                 $row.attr('aria-grabbed', true)
+//             when UP
+//                 # console.log('up', 'grabbed', $row.attr "aria-grabbed");
+//                 if $row.attr('aria-grabbed') == "true"
+//                     prev = $row.prev()
+//                     if prev
+//                         $row.insertBefore(prev)
+//                     $this.focus()
+//             when DOWN
+//                 # console.log('down', 'grabbed', $row.attr "aria-grabbed");
+//                 if $row.attr('aria-grabbed') == "true"
+//                     next = $row.next()
+//                     if next
+//                         $row.insertAfter(next)
+//                     $this.focus()
+//             when ENTER, ESC
+//                 # console.log('away', 'grabbed', $row.attr "aria-grabbed");
+//                 rows.attr("aria-grabbed", false)
+//
+// ) jQuery
+//
 
-document.addEventListener(
-  "drop",
-  function(event) {
-    // prevent default action (open as link for some elements)
-    event.preventDefault();
-    // move dragged elem to the selected drop target
-    if (event.target.className == "dropzone") {
-      event.target.style.background = "";
-      dragged.parentNode.removeChild(dragged);
-      event.target.appendChild(dragged);
-    }
-  },
-  false
-);
+// https://codepen.io/EmilyDirsh/pen/ybEQZj
