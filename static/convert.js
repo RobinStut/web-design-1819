5733d05efabda3f588a6ef682d4309151a28947c
RobinStut <robin.stut@hva.nl>
2019-04-24 11:36:54 +0200

    fixing reordering

--- static/convert.js
+++ static/convert.js
@@ -1,4 +1,3 @@
-
 (function ($) {
   const up = [192, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 189, 187, 8, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219, 221, 38];
   const down = [20, 65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 220, 16, 192, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191, 16, 17, 18, 91, 93, 18, 40];
@@ -12,6 +11,7 @@
   return grabHandle.on("keydown", function (event) {
     const $this = $(this);
     const $row = $this.parent();
+    console.log($row);
     console.log($this);
     console.log($row);
 
