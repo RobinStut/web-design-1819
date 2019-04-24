22257c695f4317789f9f44897b5dbd66a768cff6
RobinStut <robin.stut@hva.nl>
2019-04-24 09:39:00 +0200

    chanching keys

--- static/convert.js
+++ static/convert.js
@@ -1,5 +1,4 @@
 
-
 (function ($) {
   const up = [192, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 189, 187, 8, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219, 221, 38];
   const down = [20, 65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 220, 16, 192, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191, 16, 17, 18, 91, 93, 18, 40];
@@ -13,6 +12,8 @@
   return grabHandle.on("keydown", function (event) {
     const $this = $(this);
     const $row = $this.parent();
+    console.log($this);
+    console.log($row);
 
     if (event.which === 32 && spaceCount === 0) {
       spaceCount = 1;
