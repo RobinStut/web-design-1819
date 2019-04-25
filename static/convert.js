d17bd59f31cc204e6b7c24f9461c707103f669e9
Robin <robinstut@MacBook-Pro-van-Robin.local>
2019-04-25 22:11:37 +0200

    adding setTimeout

--- static/convert.js
+++ static/convert.js
@@ -83,196 +83,202 @@ content.onkeydown = function (event) {
   var rx = /(\w*\d*)(\d)_(\d*)-*(\d*)/g
   console.log(id);
   var active = document.activeElement;
-  
+
 
   if (event.keyCode == 37 || event.keyCode == 65) {
-    console.log('left');
-    if (hasParent === false) {
-      console.log('is false');
-      document.getElementById(id).blur()
-      var split = id.replace(rx, (...arg) => {
-        console.log(arg);
-        var idPlus = Number(arg[3]) - 1;
-        return `${arg[1] + arg[2] + '_' + idPlus}`
-      });
-      console.log(split);
-      document.getElementById(split).focus();
-    }
-    if (hasParent === true) {
-      console.log('is false');
-      document.getElementById(id).blur()
-      var split = id.replace(rx, (...arg) => {
-        console.log(arg);
-        var idPlus = Number(arg[4]) - 1;
-        return `${arg[1] + arg[2] + '_' + arg[3] + '-' + idPlus}`
-      });
-      console.log(split);
-      try {
+    setTimeout(function () {
+      console.log('left');
+      if (hasParent === false) {
+        console.log('is false');
+        document.getElementById(id).blur()
+        var split = id.replace(rx, (...arg) => {
+          console.log(arg);
+          var idPlus = Number(arg[3]) - 1;
+          return `${arg[1] + arg[2] + '_' + idPlus}`
+        });
+        console.log(split);
         document.getElementById(split).focus();
-      } catch (e) {
-        if (e instanceof TypeError) {
-          console.log('error');
-          split = parentId;
-          document.getElementById(split).focus();
-        }
       }
-      // document.getElementById(split).focus();
-    }
+      if (hasParent === true) {
+        console.log('is false');
+        document.getElementById(id).blur()
+        var split = id.replace(rx, (...arg) => {
+          console.log(arg);
+          var idPlus = Number(arg[4]) - 1;
+          return `${arg[1] + arg[2] + '_' + arg[3] + '-' + idPlus}`
+        });
+        console.log(split);
+        try {
+          document.getElementById(split).focus();
+        } catch (e) {
+          if (e instanceof TypeError) {
+            console.log('error');
+            split = parentId;
+            document.getElementById(split).focus();
+          }
+        }
+        // document.getElementById(split).focus();
+      }
+    }, 300)
+
   }
 
   ////////////
   else if (event.keyCode == 39 || event.keyCode == 68) {
-    console.log('right');
-    if (hasParent === false) {
-      console.log('is false');
-      document.getElementById(id).blur()
-      var split = id.replace(rx, (...arg) => {
-        var idPlus = Number(arg[3]) + 1;
-        return `${arg[1] + arg[2] + '_' + idPlus}`
-      });
-      console.log(split);
-      document.getElementById(split).focus();
-    }
-    if (hasParent === true) {
-      console.log('is false');
-      document.getElementById(id).blur()
-      var split = id.replace(rx, (...arg) => {
-        var idPlus = Number(arg[4]) + 1;
-        return `${arg[1] + arg[2] + '_' + arg[3] + '-' + idPlus}`
-      });
-      try {
+    setTimeout(function () {
+      console.log('right');
+      if (hasParent === false) {
+        console.log('is false');
+        document.getElementById(id).blur()
+        var split = id.replace(rx, (...arg) => {
+          var idPlus = Number(arg[3]) + 1;
+          return `${arg[1] + arg[2] + '_' + idPlus}`
+        });
+        console.log(split);
         document.getElementById(split).focus();
-      } catch (e) {
-        if (e instanceof TypeError) {
-          console.log('error');
-          split = parentId;
-          console.log(split);
-          document.getElementById(split).focus();
-        }
       }
-      // document.getElementById(split).focus();
-    }
+      if (hasParent === true) {
+        console.log('is false');
+        document.getElementById(id).blur()
+        var split = id.replace(rx, (...arg) => {
+          var idPlus = Number(arg[4]) + 1;
+          return `${arg[1] + arg[2] + '_' + arg[3] + '-' + idPlus}`
+        });
+        try {
+          document.getElementById(split).focus();
+        } catch (e) {
+          if (e instanceof TypeError) {
+            console.log('error');
+            split = parentId;
+            console.log(split);
+            document.getElementById(split).focus();
+          }
+        }
+        // document.getElementById(split).focus();
+      }
+    }, 300)
 
   }
   ////////////
-
   else if (event.keyCode == 38 || event.keyCode == 87) {
-    console.log('up');
+    setTimeout(function () {
+      console.log('up');
 
-    if (dragcount === 0 && classCheck==='drag') {
-      document.activeElement.blur()
-      document.getElementById('c02_1-1').focus()
-    }
+      if (dragcount === 0 && classCheck === 'drag') {
+        document.activeElement.blur()
+        document.getElementById('c02_1-1').focus()
+      }
 
-    if(classCheck === 'drag' && dragcount !== 0){
-      console.log('steen papier schaar, draggen maar!');
-      dragcount -= 1;
-      document.activeElement.blur()
-      // console.log(dragcount);
-      let a = $( "ul > li > a" )[dragcount];
-      a.focus()
-    
-    console.log(dragcount);
-    }
+      if (classCheck === 'drag' && dragcount !== 0) {
+        console.log('steen papier schaar, draggen maar!');
+        dragcount -= 1;
+        document.activeElement.blur()
+        // console.log(dragcount);
+        let a = $("ul > li > a")[dragcount];
+        a.focus()
 
-    if (hasParent === false && classCheck !== "drag") {
-      console.log('is false');
-      document.getElementById(id).blur()
-      var split = id.replace(rx, (...arg) => {
-        var idPlus = Number(arg[2]) - 1;
-        return `${arg[1] + idPlus + '_' + arg[3]}`
-      });
-      console.log(split);
-      document.getElementById(split).focus();
-    }
+        console.log(dragcount);
+      }
 
-    if (hasParent === true && classCheck!=='drag') {
-      console.log('is false');
-      document.getElementById(id).blur()
-      var split = id.replace(rx, (...arg) => {
-        // console.log(arg);
-        var idPlus = Number(arg[3]) - 1;
-        return `${arg[1] + arg[2] + '_' + idPlus + '-' + arg[4]}`
-      });
-      console.log(split);
-      try {
+      if (hasParent === false && classCheck !== "drag") {
+        console.log('is false');
+        document.getElementById(id).blur()
+        var split = id.replace(rx, (...arg) => {
+          var idPlus = Number(arg[2]) - 1;
+          return `${arg[1] + idPlus + '_' + arg[3]}`
+        });
+        console.log(split);
         document.getElementById(split).focus();
+      }
+
+      if (hasParent === true && classCheck !== 'drag') {
+        console.log('is false');
+        document.getElementById(id).blur()
+        var split = id.replace(rx, (...arg) => {
+          // console.log(arg);
+          var idPlus = Number(arg[3]) - 1;
+          return `${arg[1] + arg[2] + '_' + idPlus + '-' + arg[4]}`
+        });
+        console.log(split);
+        try {
+          document.getElementById(split).focus();
+        } catch (e) {
+          if (e instanceof TypeError) {
+            console.log('error');
+            split = parentId;
+            // console.log(split);
+            document.getElementById(split).focus();
+          }
+
+        }
+
+
+        // document.getElementById(split).focus();
+      }
+    }, 300)
+  } else if (event.keyCode == 40 || event.keyCode == 83) {
+    setTimeout(function () {
+      console.log('down');
+      var split;
+      if (hasParent === false) {
+        document.getElementById(id).blur()
+        console.log(split);
+        split = id.replace(rx, (...arg) => {
+          var idPlus = Number(arg[2]) + 1;
+          return `${arg[1] + idPlus + '_' + arg[3]}`
+        });
+        console.log(split);
+        document.getElementById(split).focus();
+      }
+
+
+
+      if (hasParent === true && classCheck !== 'drag') {
+        console.log('is false');
+        document.getElementById(id).blur()
+        split = id.replace(rx, (...arg) => {
+          var idPlus = Number(arg[3]) + 1;
+          return `${arg[1] + arg[2] + '_' + idPlus + '-' + arg[4]}`
+        });
+        console.log(split);
+      }
+      try {
+        // console.log(split);
+        console.log(document.getElementById(split));
+
+        // document.getElementById('test').focus();
+        document.getElementById(split).focus();
+        // console.log('done');
       } catch (e) {
         if (e instanceof TypeError) {
           console.log('error');
           split = parentId;
-          // console.log(split);
           document.getElementById(split).focus();
         }
       }
-
-      
-      // document.getElementById(split).focus();
-    }
-  }
-
-  else if (event.keyCode == 40 || event.keyCode == 83) {
-    console.log('down');
-    var split;
-    if (hasParent === false) {
-      document.getElementById(id).blur()
-      console.log(split);
-      split = id.replace(rx, (...arg) => {
-        var idPlus = Number(arg[2]) + 1;
-        return `${arg[1] + idPlus + '_' + arg[3]}`
-      });
-      console.log(split);
-      document.getElementById(split).focus();
-    }
-
-
-
-    if (hasParent === true && classCheck !== 'drag') {
-      console.log('is false');
-      document.getElementById(id).blur()
-      split = id.replace(rx, (...arg) => {
-        var idPlus = Number(arg[3]) + 1;
-        return `${arg[1] + arg[2] + '_' + idPlus + '-' + arg[4]}`
-      });
-      console.log(split);
-    }
-    try {
-      // console.log(split);
-      console.log(document.getElementById(split));
-
-      // document.getElementById('test').focus();
-      document.getElementById(split).focus();
-      // console.log('done');
-    } catch (e) {
-      if (e instanceof TypeError) {
-        console.log('error');
-        split = parentId;
-        document.getElementById(split).focus();
+      console.log(id);
+      if (id === "c02_1-1") {
+        console.log('hey');
+        console.log(split);
+        const $this = $(this);
+        const $row = $this.children()[1];
+        let a = $("ul > li > a")[dragcount];
+        a.focus()
+        dragcount++
+        console.log(dragcount);
       }
-    }
-    console.log(id);
-    if (id === "c02_1-1") {
-      console.log('hey');
-      console.log(split);
-      const $this = $(this);
-      const $row = $this.children()[1];
-    let a = $( "ul > li > a" )[dragcount];
-    a.focus()
-    dragcount++
-    console.log(dragcount);
-    }
 
-    if(classCheck === 'drag'){
-      console.log('steen papier schaar, draggen maar!');
-      document.activeElement.blur()
-      // console.log(dragcount);
-      let a = $( "ul > li > a" )[dragcount];
-    a.focus()
-    dragcount += 1;
-    console.log(dragcount);
-    }
+      if (classCheck === 'drag') {
+        console.log('steen papier schaar, draggen maar!');
+        document.activeElement.blur()
+        // console.log(dragcount);
+        let a = $("ul > li > a")[dragcount];
+        a.focus()
+        dragcount += 1;
+        console.log(dragcount);
+      }
 
-
+    }, 300)
   }
 }
 
