01d701851d76cfab6c2b372e29262e22893c6bcc
Robin <robinstut@216.145.028.145.hva.nl>
2019-04-25 12:34:10 +0200

    fixed drag and drop

--- static/convert.js
+++ static/convert.js
@@ -1,9 +1,71 @@
+(function ($) {
+  const up = [38];
+  const down = [40];
+
+  let spaceCount = 0;
+
+
+  const grabHandle = $("a");
+  const rows = $("li[aria-grabbed]");
+
+  return grabHandle.on("keydown", function (event) {
+    const $this = $(this);
+    const $row = $this.parent();
+    console.log($row);
+    console.log($this);
+    console.log($row);
+
+    if (event.which === 32 && spaceCount === 0) {
+      spaceCount = 1;
+      console.log(spaceCount);
+      rows.attr("aria-grabbed", false);
+      // rows.addClass("true")
+      return $row.attr('aria-grabbed', true).addClass('true');
+    }
+
+
+
+
+    if (up.includes(event.which)) {
+      if ($row.attr('aria-grabbed') === "true") {
+        const prev = $row.prev();
+        console.log(prev);
+        if (prev) {
+          $row.insertBefore(prev);
+        }
+        return $this.focus();
+      }
+    }
+
+    if (down.includes(event.which)) {
+      if ($row.attr('aria-grabbed') === "true") {
+        const next = $row.next();
+        console.log(next);
+        if (next) {
+          $row.insertAfter(next);
+        }
+        return $this.focus();
+      }
+    }
+    if (event.which === 13 || event.which === 27 || (event.which === 32 && spaceCount === 1)) {
+      spaceCount = 0;
+      rows.removeClass("true")
+      return rows.attr("aria-grabbed", false);
+    }
+  });
+
+})(jQuery);
+
+
+
+
+
 //parent
 var content = document.getElementById('content');
 // console.log(content);
 var items = content.children
 // console.log(items);
-
+var dragcount = 0;
 
 content.onkeydown = function (event) {
   // console.log(' ');
@@ -11,11 +73,17 @@ content.onkeydown = function (event) {
   let values = check(document.activeElement)
   // console.log(values);
 
+
+
   var parentId = values[2];
   var hasParent = values[1];
   var id = values[0];
+  var classCheck = document.activeElement.className;
+  console.log(classCheck);
   var rx = /(\w*\d*)(\d)_(\d*)-*(\d*)/g
-
+  console.log(id);
+  var active = document.activeElement;
+  
 
   if (event.keyCode == 37 || event.keyCode == 65) {
     console.log('left');
@@ -91,7 +159,23 @@ content.onkeydown = function (event) {
   else if (event.keyCode == 38 || event.keyCode == 87) {
     console.log('up');
 
-    if (hasParent === false) {
+    if (dragcount === 0 && classCheck==='drag') {
+      document.activeElement.blur()
+      document.getElementById('c02_1-1').focus()
+    }
+
+    if(classCheck === 'drag' && dragcount !== 0){
+      console.log('steen papier schaar, draggen maar!');
+      dragcount -= 1;
+      document.activeElement.blur()
+      // console.log(dragcount);
+      let a = $( "ul > li > a" )[dragcount];
+      a.focus()
+    
+    console.log(dragcount);
+    }
+
+    if (hasParent === false && classCheck !== "drag") {
       console.log('is false');
       document.getElementById(id).blur()
       var split = id.replace(rx, (...arg) => {
@@ -102,7 +186,7 @@ content.onkeydown = function (event) {
       document.getElementById(split).focus();
     }
 
-    if (hasParent === true) {
+    if (hasParent === true && classCheck!=='drag') {
       console.log('is false');
       document.getElementById(id).blur()
       var split = id.replace(rx, (...arg) => {
@@ -121,15 +205,19 @@ content.onkeydown = function (event) {
           document.getElementById(split).focus();
         }
       }
+
+      
       // document.getElementById(split).focus();
     }
   }
 
   else if (event.keyCode == 40 || event.keyCode == 83) {
     console.log('down');
+    var split;
     if (hasParent === false) {
       document.getElementById(id).blur()
-      var split = id.replace(rx, (...arg) => {
+      console.log(split);
+      split = id.replace(rx, (...arg) => {
         var idPlus = Number(arg[2]) + 1;
         return `${arg[1] + idPlus + '_' + arg[3]}`
       });
@@ -137,17 +225,24 @@ content.onkeydown = function (event) {
       document.getElementById(split).focus();
     }
 
-    if (hasParent === true) {
+
+
+    if (hasParent === true && classCheck !== 'drag') {
       console.log('is false');
       document.getElementById(id).blur()
-      var split = id.replace(rx, (...arg) => {
+      split = id.replace(rx, (...arg) => {
         var idPlus = Number(arg[3]) + 1;
         return `${arg[1] + arg[2] + '_' + idPlus + '-' + arg[4]}`
       });
       console.log(split);
     }
     try {
+      // console.log(split);
+      console.log(document.getElementById(split));
+
+      // document.getElementById('test').focus();
       document.getElementById(split).focus();
+      // console.log('done');
     } catch (e) {
       if (e instanceof TypeError) {
         console.log('error');
@@ -155,18 +250,44 @@ content.onkeydown = function (event) {
         document.getElementById(split).focus();
       }
     }
-    // document.getElementById(split).focus();
-  }
+    console.log(id);
+    if (id === "c02_1-1") {
+      console.log('hey');
+      console.log(split);
+      const $this = $(this);
+      const $row = $this.children()[1];
+    let a = $( "ul > li > a" )[dragcount];
+    a.focus()
+    dragcount++
+    console.log(dragcount);
+    }
 
+    if(classCheck === 'drag'){
+      console.log('steen papier schaar, draggen maar!');
+      document.activeElement.blur()
+      // console.log(dragcount);
+      let a = $( "ul > li > a" )[dragcount];
+    a.focus()
+    dragcount += 1;
+    console.log(dragcount);
+    }
+
+
+  }
 }
 
 function check(x) {
+  // console.log(check(document.activeElement));
   var parent = x.parentElement
+  // console.log(parent);
   var parentId = x.parentElement.id
   if (parentId === "") {
+    // console.log(parentId);
     parentId = x.parentElement.parentElement.id
+    // console.log(parentId);
   }
   if (parentId === "") {
+    // console.log(parentId);
     parentId = x.parentElement.parentElement.parentElement.id
   }
   var hasParent = (parentId === 'content') ? false : true;
